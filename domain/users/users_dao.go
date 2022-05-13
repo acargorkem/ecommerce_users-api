e28941c715cac254b59f51e502c4b875e83af778
acargorkem <acargorkem@outlook.com>
2022-05-13 17:52:03 +0300

    Implement create user logic on database layer

--- domain/users/users_dao.go
+++ domain/users/users_dao.go
@@ -2,10 +2,16 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
 	usersdb "github.com/acargorkem/ecommerce_users-api/datasources/postgresql/users_db"
 	dateutils "github.com/acargorkem/ecommerce_users-api/utils/date_utils"
 	"github.com/acargorkem/ecommerce_users-api/utils/errors"
+)
+
+const (
+	indexUniqueEmail = "duplicate key value violates unique constraint \"users_email_key\""
+	queryInsertUser  = "INSERT INTO users (first_name, last_name, email, created_at) VALUES ($1, $2, $3, $4) RETURNING *;"
 )
 
 var (
@@ -13,9 +19,7 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
-	if err := usersdb.Client.Ping(); err != nil {
-		panic(err)
-	}
+
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
@@ -29,17 +33,24 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	stmt, err := usersdb.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
 	user.Created_at = dateutils.GetNowString()
 
-	usersDB[user.Id] = user
+	row := stmt.QueryRow(user.FirstName, user.LastName, user.Email, user.Created_at)
+
+	err = row.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.Created_at)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(
+				fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get last insert user: %s", err.Error()))
+	}
+
 	return nil
 }
