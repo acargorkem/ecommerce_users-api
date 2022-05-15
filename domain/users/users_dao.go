079f3592f8ea705bc4756e74aaabc8aed94f6bad
acargorkem <acargorkem@outlook.com>
2022-05-15 17:36:18 +0300

    Implement error handling package for database layer

--- domain/users/users_dao.go
+++ domain/users/users_dao.go
@@ -1,12 +1,10 @@
 package users
 
 import (
-	"fmt"
-	"strings"
-
 	usersdb "github.com/acargorkem/ecommerce_users-api/datasources/postgresql/users_db"
 	dateutils "github.com/acargorkem/ecommerce_users-api/utils/date_utils"
 	"github.com/acargorkem/ecommerce_users-api/utils/errors"
+	postgresqlutils "github.com/acargorkem/ecommerce_users-api/utils/postgresql_utils"
 )
 
 const (
@@ -19,17 +17,13 @@ const (
 func (user *User) Get() *errors.RestErr {
 	stmt, err := usersdb.Client.Prepare(queryGetUser)
 	if err != nil {
-		return errors.NewInternalServerError(err.Error())
+		return postgresqlutils.ParseError(err)
 	}
 	defer stmt.Close()
 
 	row := stmt.QueryRow(user.Id)
 	if err := row.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.Created_at); err != nil {
-		if strings.Contains(err.Error(), errorNorows) {
-			return errors.NewNotFoundError(
-				fmt.Sprintf("user %d not found", user.Id))
-		}
-		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get user %d: %s", user.Id, err.Error()))
+		return postgresqlutils.ParseError(err)
 	}
 
 	return nil
@@ -38,7 +32,7 @@ func (user *User) Get() *errors.RestErr {
 func (user *User) Save() *errors.RestErr {
 	stmt, err := usersdb.Client.Prepare(queryInsertUser)
 	if err != nil {
-		return errors.NewInternalServerError(err.Error())
+		return postgresqlutils.ParseError(err)
 	}
 	defer stmt.Close()
 
@@ -48,11 +42,7 @@ func (user *User) Save() *errors.RestErr {
 
 	err = row.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.Created_at)
 	if err != nil {
-		if strings.Contains(err.Error(), indexUniqueEmail) {
-			return errors.NewBadRequestError(
-				fmt.Sprintf("email %s already exists", user.Email))
-		}
-		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get last insert user: %s", err.Error()))
+		return postgresqlutils.ParseError(err)
 	}
 
 	return nil
---
+++ utils/postgresql_utils/postgresql_utils.go
@@ -0,0 +1,29 @@
+package postgresqlutils
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/acargorkem/ecommerce_users-api/utils/errors"
+	"github.com/lib/pq"
+)
+
+const (
+	UniqueViolationError = pq.ErrorCode("23505") // 'unique_violation'
+)
+
+func ParseError(err error) *errors.RestErr {
+	if sqlErr, ok := err.(*pq.Error); ok {
+		errMessage := fmt.Sprintf("%s : %s", sqlErr.Code.Name(), sqlErr.Detail)
+		switch sqlErr.Code {
+		case UniqueViolationError:
+			return errors.NewBadRequestError(errMessage)
+		}
+
+		return errors.NewInternalServerError(errMessage)
+	}
+	if err == sql.ErrNoRows {
+		return errors.NewNotFoundError("user not found")
+	}
+	return errors.NewInternalServerError(err.Error())
+}
